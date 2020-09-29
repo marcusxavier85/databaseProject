43e2cffe8d71948d71988c679a09ad7cfc68e4c8
Marcus Bowyer <marcusxavier85@gmail.com>
2020-09-29 09:12:00 +0100

    added a background

--- pokedexDataBaseProject.php
+++ pokedexDataBaseProject.php
@@ -5,7 +5,7 @@ $db = new PDO('mysql:host=db;dbname=pokedex', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare('SELECT `id`, `pokedex_no`, `name`, `type` FROM `kanto_pokedex`;');
+$query = $db->prepare('SELECT `id`, `pokedex_no`, `name`, `type`, `img_source` FROM `kanto_pokedex`;');
 
 $query->execute();
 
@@ -26,10 +26,14 @@ $result = $query->fetchAll();
             Pokedex
         </header>
 
+        <div class="backgroundImage">
+        </div>
+
         <a href="http://localhost:1234/databaseProject/pokedexForm.php" class="addNewPokemonButtonBorder"><button class="addNewPokemonButton">Add new Pokemon</button></a>
 
         <table class="mainTable">
             <tr>
+                <th class="pokemonFont">Image</th>
                 <th class="pokemonFont">Pokedex Entry</th>
                 <th class="pokemonFont">Name</th>
                 <th class="pokemonFont">Type</th>
@@ -38,6 +42,7 @@ $result = $query->fetchAll();
         <?php
             foreach($result as $pokemon) {
                 echo "<tr>";
+                echo "<td> <img src=" . $pokemon['img_source'] . "alt=Picture of " . $pokemon['name'] . " class='pictureOfPokemon'> </td>";
                 echo "<td>" . $pokemon['pokedex_no'] . "</td>";
                 echo "<td>" . $pokemon['name'] . "</td>";
                 echo "<td>" . $pokemon['type'] . "</td>";
