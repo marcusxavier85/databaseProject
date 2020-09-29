b2df1a38f3e854ce2278eac8753b21824a4e0637
Marcus Bowyer <marcusxavier85@gmail.com>
2020-09-29 17:02:17 +0100

    final comit of the day, adding the if statement into the displayPokemon function

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  *
  * @return PDO, returning link to database
  */
-function getDatabaseObject() {
+function getDatabaseObject(): PDO {
 
     return $db = new PDO('mysql:host=db;dbname=pokedex', 'root', 'password');
 
@@ -18,10 +18,10 @@ function getDatabaseObject() {
  * Grabs the data we require from the kanto_pokedex table
  *
  * @param $db, data type is a PDO, is the link to the table
- * 
+ *
  * @return mixed, returning an associative array of all data listed in kanto_pokedex
  */
-function getPokemons($db) {
+function getPokemons(PDO $db): array {
     $query = $db->prepare('SELECT `id`, `pokedex_no`, `name`, `type`, `img_source` FROM `kanto_pokedex`;');
 
     $query->execute();
@@ -36,15 +36,19 @@ function getPokemons($db) {
  *
  * @return string, returning a concatination of all data in one array and then doing it again for the others
  */
-function displayPokemon($pokemonArray) {
-    $result = '';
-    foreach($pokemonArray as $pokemon) {
-        $result .= '<tr class="rowBox">';
-        $result .= '<td><img class="pictureOfPokemon" src=' . $pokemon['img_source'] . ' alt="Picture of "' . $pokemon['name'] . '</td>';
-        $result .= '<td>' . $pokemon['pokedex_no'] . '</td>';
-        $result .= '<td>' . $pokemon['name'] . '</td>';
-        $result .= '<td>' . $pokemon['type'] . '</td>';
-        $result .= '</tr>';
+function displayPokemon(array $pokemonArray): string {
+    if (in_array(, $pokemonArray)) {
+        $result = '';
+        foreach ($pokemonArray as $pokemon) {
+            $result .= '<tr class="rowBox">';
+            $result .= '<td><img class="pictureOfPokemon" src=' . $pokemon['img_source'] . ' alt="Picture of "' . $pokemon['name'] . '</td>';
+            $result .= '<td>' . $pokemon['pokedex_no'] . '</td>';
+            $result .= '<td>' . $pokemon['name'] . '</td>';
+            $result .= '<td>' . $pokemon['type'] . '</td>';
+            $result .= '</tr>';
+        }
+        return $result;
+    } else {
+        return 'This is not correct please reconsider';
     }
-    return $result;
 }
--- pokedexDataBaseProject.php
+++ pokedexDataBaseProject.php
@@ -9,7 +9,7 @@ $pokemons = getPokemons($db);
 
 ?>
 
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
