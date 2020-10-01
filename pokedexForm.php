766dd4ff58ac3fca2fa471f85c2f539621ddb48d
Marcus Bowyer <marcusxavier85@gmail.com>
2020-10-01 13:47:40 +0100

    i have made some of the changes you suggestted and commented the others

--- pokedexForm.php
+++ pokedexForm.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
@@ -7,42 +8,50 @@
         <link href="pokeFormStyle.css" type="text/css" rel="stylesheet">
     </head>
     <body class="pokemonFont backgroundImage">
-        <header class="">Add a new Pokemon</header>
+        <header class="formHeader">Add a new Pokemon</header>
 
         <a href="pokedexDataBaseProject.php" class="backToPokedexButton"><button>Back to Pokedex</button></a>
 
         <form method="post" action="insertPokemon.php" class="formFormat">
-            URL for Pokemon image:
-            <input type="url" name="addPokemonImage" required>
-            <br>
-            Pokedex Entry #:
-            <input type="number" min="1" max="896" name="addPokedexNo" required>
-            <br>
-            Pokemon Name:
-            <input type="string" name="addPokemonName" required>
-            <br>
-            Pokemon Type:
-            <input type="checkbox" name="addPokemonType[]" value="Normal">Normal
-            <input type="checkbox" name="addPokemonType[]" value="Fire">Fire
-            <input type="checkbox" name="addPokemonType[]" value="Fighting">Fighting
-            <input type="checkbox" name="addPokemonType[]" value="Water">Water
-            <input type="checkbox" name="addPokemonType[]" value="Flying">Flying
-            <input type="checkbox" name="addPokemonType[]" value="Grass">Grass
-            <input type="checkbox" name="addPokemonType[]" value="Poison">Poison
-            <input type="checkbox" name="addPokemonType[]" value="Electric">Electric
-            <input type="checkbox" name="addPokemonType[]" value="Ground">Ground
-            <input type="checkbox" name="addPokemonType[]" value="Psychic">Psychic
-            <input type="checkbox" name="addPokemonType[]" value="Rock">Rock
-            <input type="checkbox" name="addPokemonType[]" value="Ice">Ice
-            <input type="checkbox" name="addPokemonType[]" value="Bug">Bug
-            <input type="checkbox" name="addPokemonType[]" value="Dragon">Dragon
-            <input type="checkbox" name="addPokemonType[]" value="Ghost">Ghost
-            <input type="checkbox" name="addPokemonType[]" value="Dark">Dark
-            <input type="checkbox" name="addPokemonType[]" value="Steel">Steel
-            <input type="checkbox" name="addPokemonType[]" value="Fairy">Fairy
-            <input type="checkbox" name="addPokemonType[]" value="???">???
-            <br>
-            <input type="submit" value="submit" class="submitButton">
+            <div class="formInputs">
+                URL for Pokemon image:
+                <input type="url" name="addPokemonImage" required>
+            </div>
+            <div class="formInputs">
+                Pokedex Entry #:
+                <input type="number" min="1" max="896" name="addPokedexNo" required>
+            </div>
+            <div class="formInputs">
+                Pokemon Name:
+                <input type="string" name="addPokemonName" required>
+            </div>
+            <div class="formInputs checkboxType">
+                Pokemon Type:
+                <div class="formTypes">
+                    Normal<input type="checkbox" name="addPokemonType[]" value="Normal">
+                    Fire<input type="checkbox" name="addPokemonType[]" value="Fire">
+                    Fighting<input type="checkbox" name="addPokemonType[]" value="Fighting">
+                    Water<input type="checkbox" name="addPokemonType[]" value="Water">
+                    Flying<input type="checkbox" name="addPokemonType[]" value="Flying">
+                    Grass<input type="checkbox" name="addPokemonType[]" value="Grass">
+                    Poison<input type="checkbox" name="addPokemonType[]" value="Poison">
+                    Electric<input type="checkbox" name="addPokemonType[]" value="Electric">
+                    Ground<input type="checkbox" name="addPokemonType[]" value="Ground">
+                    Psychic<input type="checkbox" name="addPokemonType[]" value="Psychic">
+                    Rock<input type="checkbox" name="addPokemonType[]" value="Rock">
+                    Ice<input type="checkbox" name="addPokemonType[]" value="Ice">
+                    Bug<input type="checkbox" name="addPokemonType[]" value="Bug">
+                    Dragon<input type="checkbox" name="addPokemonType[]" value="Dragon">
+                    Ghost<input type="checkbox" name="addPokemonType[]" value="Ghost">
+                    Dark<input type="checkbox" name="addPokemonType[]" value="Dark">
+                    Steel<input type="checkbox" name="addPokemonType[]" value="Steel">
+                    Fairy<input type="checkbox" name="addPokemonType[]" value="Fairy">
+                    ???<input type="checkbox" name="addPokemonType[]" value="???">
+                </div>
+            </div>
+            <div class="formInputs">
+                <input type="submit" value="submit" class="submitButton">
+            </div>
         </form>
 
     </body>
